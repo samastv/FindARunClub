1fd75db7d917f0a863ae5ca4f84b4ab2b41ef052
Samast Varma <samastv@umich.edu>
2025-01-26 18:38:33 -0500

    fix time filters and ordering

--- script.js
+++ script.js
@@ -62,7 +62,24 @@ $(document).ready(function() {
                         return data;
                     }
                 },
-                { data: 'Time', width: '8%' },
+                { 
+                    data: 'Time', 
+                    width: '8%',
+                    render: function(data, type, row) {
+                        if (type === 'sort') {
+                            if (!data) return -1;
+                            let timeStr = data.trim().toUpperCase();
+                            let [time, period] = timeStr.split(' ');
+                            let [hours, minutes] = time.split(':').map(Number);
+                            
+                            if (period === 'PM' && hours !== 12) hours += 12;
+                            if (period === 'AM' && hours === 12) hours = 0;
+                            
+                            return hours * 60 + (minutes || 0);
+                        }
+                        return data;
+                    }
+                },
                 { 
                     data: 'Meet Up Information',
                     width: '32%',
@@ -80,8 +97,8 @@ $(document).ready(function() {
             scroller: false,
             pageLength: -1,
             responsive: true,
-            ordering: false,
-            order: [[0, 'asc'], [1, 'asc']],
+            ordering: true,
+            order: [[6, 'asc']],
             dom: '<"top"f>rt<"bottom"i>',
             language: {
                 info: "Showing _START_ to _END_ of _TOTAL_ clubs",
@@ -113,8 +130,13 @@ $(document).ready(function() {
             
             // Time filter
             if (time) {
-                let hour = parseInt(rowTime.split(':')[0]);
-                if (!hour) return false;
+                let timeStr = rowTime.trim().toUpperCase();
+                let hour = parseInt(timeStr);
+                let isPM = timeStr.includes('PM');
+                
+                // Convert to 24-hour format if needed
+                if (isPM && hour !== 12) hour += 12;
+                if (!isPM && hour === 12) hour = 0;
                 
                 switch(time) {
                     case 'morning':
